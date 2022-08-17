afd9657807db4f3688200e1c57e9bf8bf6572f5c
Paulo Marianno <sky.marianno@gmail.com>
2022-08-17 19:31:43 -0300

    Update IR.py

--- IR.py
+++ IR.py
@@ -7,10 +7,11 @@ base = [22847.76, 33919.8, 45012.6, 55976.16]
 ir = 0
 if (annualSalary <= base[0]):
     ir = 0
-
+    print("Voce está isento do Imposto de Renda")
+    
 if ((annualSalary > base[0]) and (annualSalary <= base[1])):
     ir = annualSalary * aliquot[0] - deduction[0]
-    print("Voce está isento do Imposto de Renda")
+    print("O valor devido a receita é de: " "%.2f" % ir)
 
 elif ((annualSalary > base[1]) and (annualSalary <= base[2])):
     ir = annualSalary * aliquot[1] - deduction[1]
